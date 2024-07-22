20fa8afea04bf8d0fee68ca0ec4b35b5a7a1df55
chinhph28756 <chinhddph28756@fpt.edu.vn>
2024-07-22 22:37:27 +0700

    save

--- src/views/TheBody.vue
+++ src/views/TheBody.vue
@@ -427,51 +427,20 @@ const fonts = ref([
     previewFont: "times",
     actions: [
       {
-        actions: [
-          [
-            {
-              action: "normalize",
-              type: "NFD",
-            },
-            {
-              action: "map_code_point",
-              map: "cu",
-            },
-            {
-              action: "shift_code_point",
-              range: [65, 90],
-              add: 119899,
-            },
-            {
-              action: "shift_code_point",
-              range: [97, 122],
-              add: 119893,
-            },
-          ],
-          [
-            {
-              action: "normalize",
-              type: "NFD",
-            },
-            {
-              action: "shift_code_point",
-              range: [65, 90],
-              add: 119951,
-            },
-            {
-              action: "shift_code_point",
-              range: [97, 122],
-              add: 119945,
-            },
-          ],
-        ],
-        action: "cycle",
+        action: "normalize",
+        type: "NFD",
+      },
+      {
+        action: "shift_code_point",
+        range: [65, 90],
+        add: 119951,
+      },
+      {
+        action: "shift_code_point",
+        range: [97, 122],
+        add: 119945,
       },
     ],
-    hasPreviewImages: false,
-    textStyle: {
-      path: "script",
-    },
   },
   {
     label: "Fraktur",
